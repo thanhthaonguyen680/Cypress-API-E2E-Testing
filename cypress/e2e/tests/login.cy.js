63a8efa27cd5c6d810ad7d3d3c8207fedeaaca44
Thảo <82259655+thanhthaonguyen680@users.noreply.github.com>
2024-03-08 09:51:32 +0700

    test

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -13,4 +13,12 @@ describe("Login Test",()=> {
         cy.get(LoginPage.passWord).clear().type(logindata.password)
         cy.get(LoginPage.loginBtn).click()
     })
+    it("TC_01_Login_Successfully",()=>{
+        cy.get(LoginPage.newUser).clear().type(logindata.username)
+        cy.get(LoginPage.passWord).clear().type(logindata.password)
+        cy.get(LoginPage.loginBtn).click()
+    })
 })
+
+
+
